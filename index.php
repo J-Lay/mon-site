f883b9caea9698c3aaddcb1ba2a3fff143e3e7f6
Jean-François LAY <j.lay782@gmail.com>
2018-05-18 10:03:11 +0200

    ajout css

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
-<<!DOCTYPE <html>
+<!DOCTYPE <html>
 <html>
     <head>
         <title>Mon SITE</title>
         <meta charset=UTF-8>
+        <style>
+            body{background: grey;}
+            p{
+                position : absolute;
+                top: 40%;
+                left: 40%;
+                font-family: Calibri;
+                font-size: 40px;
+            }
+        </style>
     </head>
-<p>Hello!!!</p>
 <body>
+<p>Hello!!!</p>
 </body>
 
-</html>>
+</html>
+
+
+git add --all && git commit -m 'ajout css'
