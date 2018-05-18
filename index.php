6f47db1b87f6d18837f37f1e407770c88727d434
Jean-François LAY <j.lay782@gmail.com>
2018-05-18 10:29:14 +0200

    ajout paragraphe + css

--- index.php
+++ index.php
@@ -1,11 +1,33 @@
-<<!DOCTYPE <html>
+<!DOCTYPE <html>
 <html>
     <head>
         <title>Mon SITE</title>
         <meta charset=UTF-8>
+        <style>
+        body{background: grey;}
+        p{
+            position: absolute;
+            top: 40%;
+            left: 40%;
+            font-family: Calibri;
+            font-size: 40px;
+        }
+        i{
+            position: absolute;
+            top: 52%;
+            left: 52%;
+            font-family: Calibri;
+            font-size: 40px;
+            color: black;}
+
+        }
+        
+        </style>
     </head>
-<p>Hello!!!</p>
 <body>
+<p>Hello!!!</p>
+
+<i>C'est vendredi</i>
 </body>
 
-</html>>
+</html>
